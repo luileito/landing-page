bcd6557d886d756b63a409fc50e2feede7092603
Luis Leiva <luileito@iti.upv.es>
2014-01-24 11:32:13 +0100

    Updated sign-up file

--- dowant.php
+++ dowant.php
@@ -1,1 +1,13 @@
-<?php // TODO: Process the sign-up form. ?>
+<?php
+// No data? Exit early.
+if ( empty($_POST) ) exit;
+
+// Verify that user has agreed.
+if ( !isset($_POST['agree']) ) die( _('You must agree to the terms and conditions!') );
+
+$name  = $_POST['name'];
+$email = $_POST['email'];
+$agree = $_POST['agree'];
+
+// Do something with the data; e.g. insert them into a DB.
+?>
